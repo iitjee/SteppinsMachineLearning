a31943d6b0feda11dd657ccafd5c24cf9968f8fc
iitjee <iit.jee.help.me@gmail.com>
2017-05-05 04:22:05 +0530

    Create 05 MNIST 01.py

---
+++ Tensorflow/01 TF Fundamentals/05 MNIST 01.py
@@ -0,0 +1,7 @@
+'''Yo! Let's start Helloworld of TF = MNIST! xD '''
+
+
+The actual code for this tutorial is very short, and all the interesting stuff happens in just three lines. However, it is 
+very important to understand the ideas behind it: both how TensorFlow works and the core machine learning concepts. Because of 
+this, we are going to very carefully work through the code.
+
